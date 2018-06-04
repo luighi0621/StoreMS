37ac5261df54be1113fdf3880aaa1fe44cbccd81
Luis Choque Castro <luis.choque@avantica.net>
2018-06-04 09:40:04 -0400

    First commit, initial project

---
+++ StoreManagement.Dal/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManagement.Dal.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Create(T add);
+        void Delete(T delete);
+        void Update(T update);
+        T Get(Expression<Func<T, bool>> condition);
+        IList<T> GetAll();
+        long Count();
+    }
+}
---
+++ StoreManagement.Dal/UserRepository.cs
@@ -0,0 +1,82 @@
+﻿using StoreManagement.Dal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace StoreManagement.Dal
+{
+    public class UserRepository
+    {
+        private StoreManagementEntities _DbContext;
+        public UserRepository()
+        {
+            _DbContext = new StoreManagementEntities();
+        }
+        public void Add(User add)
+        {
+            if (add != null)
+            {
+                try
+                {
+                    _DbContext.Users.Add(add);
+                    _DbContext.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public long Count()
+        {
+            return _DbContext.Users.LongCount();
+        }
+
+        public void Delete(User delete)
+        {
+            if (delete != null)
+            {
+                try
+                {
+                    _DbContext.Users.Remove(delete);
+                    _DbContext.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        public User Get(Expression<Func<User, bool>> condition)
+        {
+            if (condition != null)
+            {
+                var singleUser = _DbContext.Users.Where(condition).FirstOrDefault();
+                return singleUser;
+
+            }
+            return null;
+        }
+
+        public IList<User> GetAll()
+        {
+            return _DbContext.Users.ToList();
+        }
+
+        public void Update(User update)
+        {
+            try
+            {
+                _DbContext.Entry(update).State = System.Data.Entity.EntityState.Modified;
+                _DbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
